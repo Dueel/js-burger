abf11a7b9ad706be659e3d6fe2475d209475f852
Dueel <diego.bar@live.it>
2021-03-11 16:49:26 +0100

    bozza js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,32 @@
 
 var btnCalculate = document.getElementById('calculate');
 var total = document.getElementById('total');
-var ing = document.getElementsByTagName('li');
+var ingredients = document.getElementsByTagName('li');
+var totalPrice = 10;
 
-console.log(ing);
+
 
 btnCalculate.addEventListener('click', function(){
+    
 
+    for(var i = 0; i < ingredients.length; i++){
+    
+        var ing = ingredients[i];
+        var ingAdded = ing.children[2].checked;
+        var ingPrice = parseInt(ing.children[2].dataset.price);
+
+        if(ingAdded){
+
+            totalPrice += ingPrice;
+        }
+    
+    }
+
+    total.innerHTML = totalPrice;
+    console.log(totalPrice);
     
 })
+
+
+// Alert per nome panino
+// Applicare lo sconto
