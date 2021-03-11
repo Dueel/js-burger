132316af6f76c567ad3efbb052bd1753ae3db862
Dueel <diego.bar@live.it>
2021-03-11 23:54:16 +0100

    js completo

--- js/script.js
+++ js/script.js
@@ -1,29 +1,45 @@
 
-var btnCalculate = document.getElementById('calculate');
-var total = document.getElementById('total');
-var ingredients = document.getElementsByTagName('li');
-var couponUser = document.getElementById('coupon');
-var nameBurger = document.getElementById('nameBurger');
-
+var ingredients = document.getElementsByClassName('ingredients');
 var totalPrice = 10;
-var coupons = ['SCONTO1', 'SCONTO2', 'SCONTO3']
 
+var lis = document.getElementsByTagName('li');
+
+for(var i = 0; i < lis.length; i++){
+    
+    li = lis[i];
+    li.addEventListener('click', function(){
+        
+        // var clickedLi = this;
+        var clickedLiChildren = this.children;
+        var clickedCheckbox = clickedLiChildren[2];
+        console.log(clickedCheckbox);
+        clickedCheckbox.checked = !clickedCheckbox.checked
+    })
+}
+
+
+var btnCalculate = document.getElementById('calculate');
 
 btnCalculate.addEventListener('click', function(){
     
-    if(nameBurger.value != 0){
-
-        for(var i = 0; i < ingredients.length; i++){
+    var nameBurger = document.getElementById('nameBurger').value;
+    
+    if(nameBurger.length > 1){
         
+        for(var i = 0; i < ingredients.length; i++){
+            
             var ing = ingredients[i];
-            var ingAdded = ing.children[2].checked;
-            var ingPrice = parseInt(ing.children[2].dataset.price);
-    
+            var ingAdded = ing.checked;
+            var ingPrice = parseInt(ing.dataset.price);
+            
             if(ingAdded){
-    
+                
                 totalPrice += ingPrice;
             }
         }
+
+        var couponUser = document.getElementById('coupon');
+        var coupons = ['SCONTO1', 'SCONTO2', 'SCONTO3']
     
         for(var i = 0; i < coupons.length; i++){
     
@@ -32,14 +48,14 @@ btnCalculate.addEventListener('click', function(){
             }
         }
     
-        total.innerHTML = totalPrice;
-        console.log(totalPrice, couponUser.value);
-
-    } else [
+        document.getElementById('total').innerHTML = totalPrice;
+        
+    } else {
+        
         alert('Name you burger!')
-    ]
+    }
+
 })
 
 
-// Alert per nome panino
-// Applicare lo sconto
+
